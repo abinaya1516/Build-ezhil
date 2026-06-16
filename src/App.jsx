dbdfdbe728dc711e55dc72c557a9c7365b9a49cd
Abinaya B <abinayabalamurugan1516@gmail.com>
2026-06-16 21:07:09 +0530

    test app render

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Report from "./pages/Report";
-import Dashboard from "./pages/Dashboard";
-import Assistant from "./pages/Assistant";
-import Auth from "./pages/Auth";
-import Profile from "./pages/Profile";
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="report" element={<Report />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="assistant" element={<Assistant />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </Router>
-  );
+    <div>
+      <h1>Madurai Ezhil Working ✅</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
